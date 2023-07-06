08cb3b606574acf094e6b237714db1d392d801d3
joaopaulocod <joaopaulocod>
2023-07-06 01:30:21 +0000

    mountpoint_manipula_strutct
    Criamos o mountpoint com o dls e aprendemos a manipular struct

--- enviroment/mount/mountpoint.py
+++ enviroment/mount/mountpoint.py
@@ -23,10 +23,17 @@ scopo-kv-bootcamp-prd-wsy
 # COMMAND ----------
 
 dbutils.fs.mount(
-    source = "wasbs://sandbox@dlsbootcampprdwsy.blob.core.windows.net"
+    source = "wasbs://sandbox@dlsbootcampprdjpm.blob.core.windows.net"
     ,mount_point = "/mnt/sandbox/"
-    ,extra_configs = {"fs.azure.account.key.dlsbootcampprdwsy.blob.core.windows.net" :dbutils.secrets.get(scope = "scopo-kv-bootcamp-prd-wsy", key = "secret-dlsbootcampprdwsy")}
+    ,extra_configs = {"fs.azure.account.key.dlsbootcampprdjpm.blob.core.windows.net" :dbutils.secrets.get(scope = "scope-bootcamp-jpm", key = "secret-dlsbootcampjpm")}
 )
+
+# COMMAND ----------
+
+dbutils.fs.mount(
+source = "wasbs://raw@dlsbootcampprdjpm.blob.core.windows.net"
+,mount_point = "/mnt/raw/"
+,extra_configs = {"fs.azure.account.key.dlsbootcampprdjpm.blob.core.windows.net" :dbutils.secrets.get(scope = "scope-bootcamp-jpm", key = "secret-dlsbootcampprdjpm")})
 
 # COMMAND ----------
 
@@ -46,13 +53,13 @@ dbutils.fs.unmount( f"/mnt/transient/")
 
 # COMMAND ----------
 
-config =  {"fs.azure.account.key.dlsbootcampprdwsy.blob.core.windows.net" :dbutils.secrets.get(scope = "scopo-kv-bootcamp-prd-wsy", key = "secret-dlsbootcampprdwsy")}
+config =  {"fs.azure.account.key.dlsbootcampprdjpm.blob.core.windows.net" :dbutils.secrets.get(scope = "scope-bootcamp-jpm", key = "secret-dlsbootcampjpm")}
 
 # COMMAND ----------
 
 # DBTITLE 1,Lista de diretórios do lake 
 #apenas os diretórios que vamos interagir
-diretorios = ['transient','raw','delta','sandbox','gerencia']
+diretorios = ['transient','raw']
 
 # COMMAND ----------
 
@@ -61,7 +68,7 @@ def mount_diretorio_lake(lst_diretorios):
     try:        
         for diretorio in lst_diretorios:
             dbutils.fs.mount(
-                source = f"wasbs://{diretorio}@dlsbootcampprdwsy.blob.core.windows.net"
+                source = f"wasbs://{diretorio}@dlsbootcampprdjpm.blob.core.windows.net"
                 ,mount_point = f"/mnt/{diretorio}/"
                 ,extra_configs = config
             )
